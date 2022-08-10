cdb043aff857ce30baf618028d86be640cd47156
Daniel conteh <contehdaniel1995@gmail.com>
2022-08-10 04:43:37 +0000

    one

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import * as style from '../../style/footer.module.scss'
+import { StaticImage } from "gatsby-plugin-image"
+
 
 const Footer = () => {
   return (
@@ -10,7 +12,27 @@ const Footer = () => {
           <span>&copy; </span>copyright {new Date().getFullYear()}
         </p>
       </div>
-      <div className={style.footer_Auther}>Daniel conteh</div>
+      <div className={style.footer_Auther}>
+        <div className={style.footer_Auther_img}>
+            <StaticImage
+            src="../../images/me.jpg"
+            alt="footer_Auther_img"
+            className={style.about_img}
+            placeholder="blurred"
+            layout="constrained"
+            objectFit="contain"
+            width={150}
+            height={150}
+            style={{
+              // mixBlendMode:'multiply'
+              borderRadius: '50%',
+              marginRight: '1rem',
+              objectFit: 'cover',
+            }}
+          ></StaticImage>
+        </div>
+        <p>Daniel conteh</p>
+      </div>
     </div>
   )
 }
