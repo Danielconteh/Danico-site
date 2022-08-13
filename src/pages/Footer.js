f33fc54521ed552d0cd4a567cc2410ac1f70e571
Daniel conteh <contehdaniel1995@gmail.com>
2022-08-13 04:36:07 +0000

    style update...

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import * as Style from '../../style/footer.module.scss'
+import * as style from '../../style/footer.module.scss'
 import { StaticImage } from "gatsby-plugin-image"
 
 
 const Footer = () => {
   return (
-    <div className={Style.footer_container}>
-      <div className={Style.footer_info}>
+    <div className={style.footer_container}>
+      <div className={style.footer_info}>
         <p>
           {' '}
           <span>&copy; </span>copyright {new Date().getFullYear()}
         </p>
       </div>
-      <div className={Style.footer_Auther}>
-        <div className={Style.footer_Auther_img}>
+      <div className={style.footer_Auther}>
+        <div className={style.footer_Auther_img}>
             <StaticImage
             src="../../images/footer_img.jpg"
             alt="footer_Auther_img"
